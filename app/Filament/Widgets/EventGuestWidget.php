35b32ccda798c49e7c1274bc5ccd24b2fa4c06cb
livghit <liviu.g01@gmail.com>
2025-01-12 10:49:51 +0100

    admin removed total guest at both events removed is not relevant

--- app/Filament/Widgets/EventGuestWidget.php
+++ app/Filament/Widgets/EventGuestWidget.php
@@ -3,15 +3,13 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Event;
-use Faker\Core\Number;
+use Filament\Tables\Table;
 use Filament\Widgets\StatsOverviewWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
-use phpDocumentor\Reflection\Types\Integer;
 
 class EventGuestWidget extends StatsOverviewWidget
 {
     protected static ?string $heading = 'Chart';
-
 
     private static function getTotalGuestsForEvent(Event $event): int
     {
@@ -24,27 +22,16 @@ class EventGuestWidget extends StatsOverviewWidget
         return $totalPersons;
     }
 
-    protected function getAllGuests(): int
-    {
-        $weddingGuests = $this->getTotalGuestsForEvent(Event::find(1));
-        $standesamtGuests = $this->getTotalGuestsForEvent(Event::find(2));
-        return $standesamtGuests + $weddingGuests;
-    }
-
     protected function getStats(): array
     {
         return [
-            Stat::make('Guests at ' . Event::find(1)->name, $this->getTotalGuestsForEvent(Event::find(1)))
+            Stat::make('Guests at '.Event::find(1)->name, $this->getTotalGuestsForEvent(Event::find(1)))
                 ->description('Guests that accepted the event'),
-            Stat::make('Guests at ' . Event::find(2)->name, $this->getTotalGuestsForEvent(Event::find(2)))
+            Stat::make('Guests at '.Event::find(2)->name, $this->getTotalGuestsForEvent(Event::find(2)))
                 ->description('Guests that accepted the event'),
-            Stat::make('Total Guests both Events', $this->getAllGuests())
-                ->description('total Events Guests'),
             Stat::make('Event Location Costs', \Illuminate\Support\Number::currency($this->getTotalGuestsForEvent(Event::find(2)) * 150, 'EUR'))
                 ->description('Total location costs in €')
                 ->icon('heroicon-s-currency-dollar'),
         ];
     }
-
-
 }
