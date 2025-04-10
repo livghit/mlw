78d1c22e1ac3d0381745a66d682b8ecd6c7d76f1
livghit <liviu.g01@gmail.com>
2025-04-10 18:53:58 +0200

    ⛏️ wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -175,7 +175,7 @@
                 class="relative flex h-full transform items-end rounded-md border-2 border-black bg-white transition-transform group-hover:-translate-x-2 group-hover:-translate-y-2">
                 <div class="p-4 !pt-0 transition-opacity group-hover:absolute group-hover:opacity-0 sm:p-6 lg:p-8">
                     <svg xmlns="http://www.w3.org/2000/svg" class="size-10 sm:size-12 " fill="none"
-                         viewBox="0 0 24 24" stroke-width="0.5" stroke="currentColor">
+                         viewBox="0 0 24 24" stroke-width="1" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round"
                               d="M21 11.25v8.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5v-8.25M12 4.875A2.625 2.625 0 1 0 9.375 7.5H12m0-2.625V7.5m0-2.625A2.625 2.625 0 1 1 14.625 7.5H12m0 0V21m-8.625-9.75h18c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125h-18c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z"/>
                     </svg>
